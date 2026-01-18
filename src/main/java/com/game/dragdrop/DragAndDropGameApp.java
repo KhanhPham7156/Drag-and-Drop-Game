2d038c3b37daaa751b573394fb3d8afd6eac4838
Khánh Phạm <phamsonggiakhanh2@gmail.com>
2026-01-18 16:48:15 +0700

    initial commit

---
+++ src/main/java/com/game/dragdrop/DragAndDropGameApp.java
@@ -0,0 +1,13 @@
+package com.game.dragdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragAndDropGameApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragAndDropGameApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/dragdrop/DragAndDropGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.dragdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragAndDropGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
